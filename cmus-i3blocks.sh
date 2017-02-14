4f19446326d83d8df0189c14ee3a66c0dde2b0f3
chrislovessushi <chrislovessushi@gmail.com>
2017-02-13 23:06:35 -0500

    latest

--- cmus-i3blocks.sh
+++ cmus-i3blocks.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 artist=$(cmus-remote -Q | grep ' artist ' | cut -d ' ' -f3-)
 song=$(cmus-remote -Q | grep title | cut -d ' ' -f3-)
-echo " $artist -  $song  $line"
+echo " $artist $song  $line"
