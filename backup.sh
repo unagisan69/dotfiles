06331f28678bd199b882bc09a2b81fdfbf2f47ba
chrislovessushi <chrislovessushi@gmail.com>
2017-09-01 23:26:26 -0400

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Author: Chris White
+# ssh-keygen and ssh-copy-id on server first
+
+NOW=$(date +"%m-%d-%Y")
+FILE="sitename-$NOW.tar"
+BACKUP_DIR="/home/user/backups"
+WWW_DIR="/home/user/sitename.com/"
+FINAL="$FILE.gz"
+DB_USER=""
+DB_PASS=""
+DB_NAME=""
+DB_FILE="sitename.$NOW.sql"
+
+# Tar transforms for better archive structure.
+# WWW_TRANSFORM='s,^home/username/www/example.org,www,'
+# DB_TRANSFORM='s,^home/username/backups,database,'
+# Create the archive and the MySQL dump
+tar -cf $BACKUP_DIR/$FILE $WWW_DIR
+mysqldump -u$DB_USER -p$DB_PASS -$DB_NAME > $BACKUP_DIR/$DB_FILE
+
+# Append the dump to the archive, remove the dump and compress the whole archive.
+tar --append --file=$BACKUP_DIR/$FILE $BACKUP_DIR/$DB_FILE
+rm $BACKUP_DIR/$DB_FILE
+gzip -9 $BACKUP_DIR/$FILE
+rsync --progress $BACKUP_DIR/$FINAL user@host:/home/user/backups/sites/sitename/
